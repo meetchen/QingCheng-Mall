88bcf65a2c87caef6f19454e72826b9a2c793bcd
meetchen <17408070327@stu.xust.edu.cn>
2019-10-28 15:29:21 +0800

    commit again again

--- qingchengmall_web_portal/src/main/java/com/qingcheng/controller/ItemController.java
+++ qingchengmall_web_portal/src/main/java/com/qingcheng/controller/ItemController.java
@@ -1,16 +1,18 @@
 package com.qingcheng.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.qingcheng.pojo.goods.Goods;
 import com.qingcheng.pojo.goods.Sku;
 import com.qingcheng.pojo.goods.Spu;
+import com.qingcheng.service.goods.CategoryService;
 import com.qingcheng.service.goods.SpuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.NativeWebRequest;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
@@ -18,6 +20,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,7 +30,9 @@ import java.util.Map;
 public class ItemController {
     @Reference
     private SpuService spuService;
-    @Value("$(pagePath)")
+    @Reference
+    private CategoryService categoryService;
+    @Value("${pagePath}")
     private String pagePath;
     @Autowired
     private TemplateEngine templateEngine;
@@ -40,6 +45,18 @@ public class ItemController {
         Spu spu = goodsById.getSpu();
         //获取sku列表
         List<Sku> skuList = goodsById.getSkuList();
+        //查询商品分类
+        List<String> categoryList =new ArrayList<>();
+        categoryList.add(categoryService.findById(spu.getCategory1Id()).getName());
+        categoryList.add(categoryService.findById(spu.getCategory2Id()).getName());
+        categoryList.add(categoryService.findById(spu.getCategory3Id()).getName());
+        //sku地址列表
+        Map<String, String> urlMap = new HashMap<>();
+        for (Sku sku : skuList) {
+            String jsonString = JSON.toJSONString(JSON.parseObject(sku.getSpec()), SerializerFeature.MapSortField);
+            urlMap.put(jsonString,sku.getId()+".html");
+        }
+
         //批量生成页面
         for (Sku sku : skuList) {
             // 1.创建上下文和数据模型
@@ -47,6 +64,35 @@ public class ItemController {
             Map dateModel = new HashMap<>();
             dateModel.put("spu",spu);
             dateModel.put("sku",sku);
+            dateModel.put("categoryList",categoryList);
+            dateModel.put("skuImages",sku.getImage().split(","));
+            dateModel.put("spuImages",spu.getImage().split(","));
+            //参数列表
+            Map paraItems = JSON.parseObject(spu.getParaItems());
+            dateModel.put("paraItems",paraItems);
+            //规格列表
+            Map<String,String> specItems = (Map) JSON.parseObject(sku.getSpec());
+            dateModel.put("specItems",specItems);
+            Map<String,List> specMap = (Map)JSON.parseObject(sku.getSpec());
+            for (String key : specMap.keySet()) {
+                List<String> list = specMap.get(key);
+                List<Map> mapList = new ArrayList<>();
+                for (String value : list) {
+                    Map map = new HashMap();
+                    map.put("option",value);
+                    if (specItems.get(key).equals(value)){
+                        map.put("checked",true);
+                    }else {
+                        map.put("checked",false);
+                    }
+                    Map<String,String> spec = (Map)JSON.parseObject(sku.getSpec());
+                    spec.put(key,value);
+                    String jsonString = JSON.toJSONString(spec, SerializerFeature.MapSortField);
+                    map.put("url",urlMap.get(jsonString));
+                    mapList.add(map);
+                }
+                specMap.put(key,mapList);
+            }
             context.setVariables(dateModel);
             // 2.准备文件
             File dir = new File(pagePath);
@@ -60,9 +106,7 @@ public class ItemController {
                 PrintWriter writer = new PrintWriter(dest,"UTF-8");
                 templateEngine.process("item",context,writer);
                 System.out.println("生成页面"+sku.getId()+".html");
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (UnsupportedEncodingException e) {
+            } catch (FileNotFoundException | UnsupportedEncodingException e) {
                 e.printStackTrace();
             }
         }
